2fb3cedef69ffdb2706ee3b93c5cc9c64b7a5ad7
Yuri Bounich <b52ybunich@gmail.com>
2024-10-28 22:22:37 +0200

    add FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,16 +1,13 @@
+import FriendListItem from '../FriendListItem/FriendListItem';
 import s from './FriendList.module.css';
 
 const FriendList = ({ friends }) => {
   return (
     <div className={s.cotainer}>
       <ul className={s.block}>
-        {friends.map(item => (
-          <li className={s.item} key={item.id}>
-            <img src={item.avatar} alt="Avatar" width="48" />
-            <h2>{item.name}</h2>
-            <p className={item.isOnline ? s.online : s.offline}>
-              {item.isOnline ? 'Online' : 'Offline'}
-            </p>
+        {friends.map(({ id, avatar, name, isOnline }) => (
+          <li className={s.item} key={id}>
+            <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
           </li>
         ))}
       </ul>
